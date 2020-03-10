cd7802822b44b9343c53e5bbdf74d7f8dc0f9fae
Charles Tolentino <ctol1994@gmail.com>
2020-03-09 21:23:03 -0700

    code cleanup and commenting

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+// Creating Robot image element
 const robot = document.createElement("img");
 robot.src = "robot.png";
 robot.id = 'robot';
 robot.style.width = "50px";
 
-let xPosition = 0;
-let yPosition = 0;
-let angle = 180;
+// Initializing coordinates
+let x = 0;
+let y = 0;
+let f = 180;
 
+// Directions for Report and Output 
 let facings = {
     0: 'South',
     90: 'West',
@@ -14,9 +17,12 @@ let facings = {
     270: 'East'
 }
 
+// Boolean if Robot was active at least once
 let robotWasActivated = false;
+// Boolean if Robot is currently active
 let robotActive = false;
 
+// Setting a rounded border which is used to indicate the direction the Robot is facing
 const setFace = () => {
     let facingIndicator = {
         0: 'border-bottom',
@@ -25,112 +31,100 @@ const setFace = () => {
         270: 'border-right'
     }
     if(!robot.classList.length) {
-        robot.classList.add('border-primary','rounded-circle',facingIndicator[angle]);
+        robot.classList.add('border-primary','rounded-circle',facingIndicator[f]);
     } else {
-        let f = robot.classList.item(2);
-        if(f !== facingIndicator[angle]) {
-            robot.classList.remove(f);
-            robot.classList.add(facingIndicator[angle]);
+        let face = robot.classList.item(2);
+        if(face !== facingIndicator[f]) {
+            robot.classList.remove(face);
+            robot.classList.add(facingIndicator[f]);
         }
     }
 }
 
-const placeRobot = () => {
-    let x = Number(document.getElementById('x').value);
-    let y = Number(document.getElementById('y').value);
-    let f = Number(document.getElementById('f').value);
-    place(x,y,f);
+// Place function to set Robot onto table
+const place = () => {
+    x = Number(document.getElementById('x').value);;
+    y = Number(document.getElementById('y').value);
+    f = Number(document.getElementById('f').value);
     setFace();
     robotActive = true;
     robotWasActivated = true;
-    document.getElementById(`${xPosition}${yPosition}`).appendChild(robot);
-
+    document.getElementById(`${x}${y}`).appendChild(robot);
+    // Enabling other command buttons
     let robotCommands = document.getElementsByClassName('robot-commands');
     for(let i = 0; i < robotCommands.length; i++) {
         robotCommands[i].disabled = false;
     }
+    console.log(`PLACE ${x},${y},${facings[f]}`);
 }
 
-const place = (x,y,f) => {
-    xPosition = x;
-    yPosition = y;
-    angle = f;
-    console.log(`PLACE ${x},${y},${facings[angle]}`);
-}
-
+// Move function to move Robot by one unit depending on direction facing
 const move = () => {
-    switch(angle) {
-        case 0: // South
-            if(yPosition !== 0) {
-                document.getElementById(`${xPosition}${yPosition}`).removeChild(robot);
-                yPosition--;
-                document.getElementById(`${xPosition}${yPosition}`).appendChild(robot);
-            }
-            break;
-        case 90: // West
-            if(xPosition !== 0) {
-                document.getElementById(`${xPosition}${yPosition}`).removeChild(robot);
-                xPosition--;
-                document.getElementById(`${xPosition}${yPosition}`).appendChild(robot);
-            }            
-            break;
-        case 180: // North
-            if(yPosition !== 4) {
-                document.getElementById(`${xPosition}${yPosition}`).removeChild(robot);
-                yPosition++;
-                document.getElementById(`${xPosition}${yPosition}`).appendChild(robot);
-            }
-            break;
-        case 270: // East
-            if(xPosition !== 4) {
-                document.getElementById(`${xPosition}${yPosition}`).removeChild(robot);
-                xPosition++;
-                document.getElementById(`${xPosition}${yPosition}`).appendChild(robot);
-            }
-            break;
-    }
-    console.log('MOVE');
-}
-
-const left = () => {
-    angle = angle === 0 ? 270 : angle-90;
-    setFace();
-    console.log('LEFT');
-}
-
-const right = () => {
-    angle = angle === 270 ? 0 : angle+90;
-    setFace();
-    console.log('RIGHT');
-}
-
-const report = () => {
-    document.getElementById('xReport').textContent = xPosition;
-    document.getElementById('yReport').textContent = yPosition;
-    document.getElementById('fReport').textContent = facings[angle];
-    document.getElementById('reportContainer').classList.remove('d-none');
-    console.log(`REPORT\nOutput: ${xPosition},${yPosition},${facings[angle]}`);
-}
-
-document.onkeydown = e => {
-    if(e.keyCode === 32) {
-        document.getElementById('place').click();
-    } else {
-        if(robotActive && robotWasActivated) {
-            switch (e.keyCode) {
-                case 37:
-                    document.getElementById('left').click();
-                    break;
-                case 38:
-                    document.getElementById('move').click();
-                    break;
-                case 39:
-                    document.getElementById('right').click();
-                    break;
-                case 40:
-                    document.getElementById('report').click();
-                    break;
-            }
+    if(robotActive && robotWasActivated) {
+        switch(f) {
+            case 0: // South
+                if(y !== 0) {
+                    document.getElementById(`${x}${y}`).removeChild(robot);
+                    y--;
+                    document.getElementById(`${x}${y}`).appendChild(robot);
+                }
+                break;
+            case 90: // West
+                if(x !== 0) {
+                    document.getElementById(`${x}${y}`).removeChild(robot);
+                    x--;
+                    document.getElementById(`${x}${y}`).appendChild(robot);
+                }            
+                break;
+            case 180: // North
+                if(y !== 4) {
+                    document.getElementById(`${x}${y}`).removeChild(robot);
+                    y++;
+                    document.getElementById(`${x}${y}`).appendChild(robot);
+                }
+                break;
+            case 270: // East
+                if(x !== 4) {
+                    document.getElementById(`${x}${y}`).removeChild(robot);
+                    x++;
+                    document.getElementById(`${x}${y}`).appendChild(robot);
+                }
+                break;
         }
+        console.log('MOVE');
     }
-};
+}
+
+// Left function to rotate Robot counter-clockwise
+const left = () => {
+    if(robotActive && robotWasActivated) {
+        f = f === 0 ? 270 : f-90;
+        setFace();
+        console.log('LEFT');
+    }
+}
+
+// Right function to rotate Robot clockwise
+const right = () => {
+    if(robotActive && robotWasActivated) {
+        f = f === 270 ? 0 : f+90;
+        setFace();
+        console.log('RIGHT');
+    }
+}
+
+// Report function to display Robot's current position
+const report = () => {
+    if(robotActive && robotWasActivated) {
+        document.getElementById('xReport').textContent = x;
+        document.getElementById('yReport').textContent = y;
+        document.getElementById('fReport').textContent = facings[f];
+        document.getElementById('reportContainer').classList.remove('d-none');
+        console.log(`REPORT\nOutput: ${x},${y},${facings[f]}`);
+    }
+}
+
+// Handle closing Place Robot modal when clicking cancel, x, or outside and whether or not there's an active robot
+$('#placeRobotModal').on('hidden.bs.modal', () => {
+    robotActive = robotWasActivated ? true : false;
+});
